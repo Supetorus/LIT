c3c3acffb9b08a52a2db61c695f8685a5ad6da9e
Supetorus <littlewilliamthomas@gmail.com>
2022-11-01 13:52:25 -0600

    Adds common direct X includes into a single header file.

---
+++ Direct3DRenderer/source/renderer/DX.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "d3d11.h"
+#include "directxmath.h"
+#include "dxgidebug.h"
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "D3DCompiler.lib")
+namespace dx = DirectX;
