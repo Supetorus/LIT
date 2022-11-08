2aeeb64051d9c3d2bf4128b3d6e029747865c738
Supetorus <littlewilliamthomas@gmail.com>
2022-11-08 10:10:28 -0700

    Can control camera

--- Direct3DRenderer/source/Camera.cpp
+++ Direct3DRenderer/source/Camera.cpp
@@ -1,13 +1,39 @@
 #include "Camera.h"
+#include "Input.h"
 
 namespace wl
 {
 
-	Camera::Camera(const Object &parent, float width, float height):
-		perspectiveWidth(width),
-		perspectiveHeight(height),
-		Component(parent)
-	{}
+	//Camera::Camera(const Object &parent, float width, float height):
+	//	perspectiveWidth(width),
+	//	perspectiveHeight(height),
+	//	Component(parent)
+	//{}
+
+	void Camera::Update(float dt)
+	{
+		float speed = 1.0f;
+		const char space = 0x20;
+		const char shift = 0x10;
+		if (Input::Instance.GetKeyDown('A'))	transform.posX -= speed * dt;
+		if (Input::Instance.GetKeyDown('D'))	transform.posX += speed * dt;
+		if (Input::Instance.GetKeyDown('W'))	transform.posZ += speed * dt;
+		if (Input::Instance.GetKeyDown('S'))	transform.posZ -= speed * dt;
+		if (Input::Instance.GetKeyDown(space))	transform.posY += speed * dt;
+		if (Input::Instance.GetKeyDown(shift))	transform.posY -= speed * dt;
+
+		float rSpeed = 1.0f;
+		const char left = 0x25;
+		const char right = 0x27;
+		const char up = 0x26;
+		const char down = 0x28;
+		if (Input::Instance.GetKeyDown(left))	transform.angleY -= speed * dt;
+		if (Input::Instance.GetKeyDown(right))	transform.angleY += speed * dt;
+		if (Input::Instance.GetKeyDown(up))		transform.angleX -= speed * dt;
+		if (Input::Instance.GetKeyDown(down))	transform.angleX += speed * dt;
+
+
+	}
 
 	void Camera::SetNearPlane(float distance)
 	{
--- Direct3DRenderer/source/Camera.h
+++ Direct3DRenderer/source/Camera.h
@@ -1,23 +1,24 @@
 #pragma once
 #include "Transform.h"
-#include "Component.h"
-#include "Object.h"
+//#include "Component.h"
+//#include "Object.h"
 
 namespace wl
 {
-
-	class Camera: public Component
+	class Transform;
+	class Camera//: public Component
 	{
 	public:
-		Camera() = delete;
-		Camera(const Object &parent, float width, float height);
+		Camera() = default;
+		void Update(float dt);
+		//Camera(const Object &parent, float width, float height);
 		void SetNearPlane(float distance);
 		void SetFarPlane (float distance);
-	private:
+		Transform transform;
 		float perspectiveWidth = 1;
 		float perspectiveHeight = 1.333333f;
 		float nearPlane = 0.5f;
-		float farPlane = 10.0f;
+		float farPlane = 50.0f;
 	};
 
 }
