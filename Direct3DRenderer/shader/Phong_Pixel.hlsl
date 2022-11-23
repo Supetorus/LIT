c0bc554624a908d6839e7ebfd273455338eb25c1
Supetorus <littlewilliamthomas@gmail.com>
2022-11-23 13:33:32 -0700

    Diffuse lighting

--- Direct3DRenderer/shader/Phong_Pixel.hlsl
+++ Direct3DRenderer/shader/Phong_Pixel.hlsl
@@ -1,4 +1,41 @@
-float4 main() : SV_TARGET
+Texture2D tex;
+
+SamplerState splr;
+
+cbuffer lights 
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	float3 lightPos;
+}
+
+static const float3 materialColor = { 0.7f, 0.7f, 0.9f };
+static const float3 ambient = { 0.05f, 0.05f, 0.05f };
+static const float3 diffuseColor = { 1.0f, 1.0f, 1.0f };
+static const float diffuseIntensity = 1.0f;
+static const float attConst = 1.0f;
+static const float attLin = 0.045f;
+static const float attQuad = 0.0075f;
+
+struct PSInput
+{
+	float4 pos : SV_Position;
+	float2 tex : TEXCOORD;
+	float3 normal : NORMAL;
+	float3 worldPos : POSITION; // fix this (where does it come from?)
+};
+
+float4 main(PSInput input) : SV_TARGET
+{
+	const float3 vToL = lightPos - input.worldPos;
+	const float distToL = length(vToL);
+	const float3 dirToL = vToL / distToL;
+	
+	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
+	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, input.normal));
+	//const float3 diffuse = tex.Sample(splr, input.tex) * diffuseIntensity * att * max(0.0f, dot(dirToL, input.normal));
+	return float4(saturate(diffuse + ambient) * tex.Sample(splr, input.tex), 1.0f);
+	
+	
+	
+	//return tex.Sample(splr, input.tex);
+
 }
--- Direct3DRenderer/shader/Phong_Vertex.hlsl
+++ Direct3DRenderer/shader/Phong_Vertex.hlsl
@@ -1,4 +1,29 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+cbuffer CameraBuf : register(b0)
 {
-	return pos;
+	float4x4 viewProjection;
+};
+
+cbuffer ObjBuf : register(b1)
+{
+	float4x4 objTransform;
+};
+
+struct VSOut
+{
+	float4 pos : SV_Position;
+	float2 tex : TEXCOORD;
+	float3 normal : NORMAL;
+	float3 worldPos : POSITION;
+};
+
+VSOut main(float3 pos : POSITION, float2 tex : TEXCOORD, float3 normal : NORMAL)
+{
+	VSOut vso;
+	float4x4 mvp = mul(viewProjection, objTransform);
+	vso.pos = mul(mvp, float4(pos, 1.0f)); // Convert the pos to mvp space
+	vso.tex = tex;
+	//vso.normal = mul((float3x3) objTransform, normal); // convert the normal vector to mvp space
+	vso.normal = normalize(mul((float3x3) objTransform, normal)); // convert the normal vector to mvp space
+	vso.worldPos = (float3) mul(objTransform, float4(pos, 1.0f)); // get the world position of the vertex
+	return vso;
 }
