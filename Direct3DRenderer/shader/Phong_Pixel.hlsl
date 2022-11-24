7989e9e0578cb5acec8c0b0de74db349eaf499a9
Supetorus <littlewilliamthomas@gmail.com>
2022-11-23 21:19:21 -0700

    Moves constants from being declared in shader to being given by cbuffer.

--- Direct3DRenderer/shader/Phong_Pixel.hlsl
+++ Direct3DRenderer/shader/Phong_Pixel.hlsl
@@ -5,21 +5,21 @@ SamplerState splr;
 cbuffer light
 {
 	float3 lightPos;
+	float diffuseIntensity;
+	float attConst;
+	float attLin;
+	float attQuad;
 }
 
 static const float3 ambient = { 0.05f, 0.05f, 0.05f };
 static const float3 diffuseColor = { 1.0f, 1.0f, 1.0f };
-static const float diffuseIntensity = 1.0f;
-static const float attConst = 1.0f;
-static const float attLin = 0.045f;
-static const float attQuad = 0.0075f;
 
 struct PSInput
 {
 	float4 pos : SV_Position;
 	float2 tex : TEXCOORD;
 	float3 normal : NORMAL;
-	float3 worldPos : POSITION; // fix this (where does it come from?)
+	float3 worldPos : POSITION;
 };
 
 float4 main(PSInput input) : SV_TARGET
@@ -30,11 +30,5 @@ float4 main(PSInput input) : SV_TARGET
 	
 	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, input.normal));
-	//const float3 diffuse = tex.Sample(splr, input.tex) * diffuseIntensity * att * max(0.0f, dot(dirToL, input.normal));
 	return float4(saturate(diffuse + ambient) * (float3)tex.Sample(splr, input.tex), 1.0f);
-	
-	
-	
-	//return tex.Sample(splr, input.tex);
-
 }
