e3b2cf6675841f22b35a1092dd16e106b208c217
Supetorus <littlewilliamthomas@gmail.com>
2022-12-06 13:08:01 -0700

    restructure PointLightList class

--- Direct3DRenderer/source/PointLightList.cpp
+++ Direct3DRenderer/source/PointLightList.cpp
@@ -7,33 +7,7 @@ namespace wl
 	
 	PointLightList::PointLightList()
 	{
-		D3D11_BUFFER_DESC sbufferDesc = {  };
-		sbufferDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-		sbufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-		sbufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-		sbufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
-		sbufferDesc.ByteWidth = sizeof(PointLight) * 2;
-		sbufferDesc.StructureByteStride = sizeof(PointLight);
-
-		D3D11_SUBRESOURCE_DATA sbufferData{};
-		sbufferData.pSysMem = new byte[sizeof(PointLight)]{0};
-
-		ASSERT_HR(
-			DXContext::Instance->m_device->CreateBuffer(&sbufferDesc, &sbufferData, &m_buffer),
-			"Unable to create buffer."
-		);
-
-		// Make view
-		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
-		srvDesc.Format = DXGI_FORMAT_UNKNOWN;
-		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
-		srvDesc.Buffer.FirstElement = 0;
-		srvDesc.Buffer.NumElements = 1;
-
-		ASSERT_HR(
-			DXContext::Instance->m_device->CreateShaderResourceView(m_buffer.Get(), &srvDesc, &m_view),
-			"Unable to get shader resource view."
-		);
+		
 	}
 	
 	void PointLightList::Add(PointLight& light)
@@ -51,26 +25,81 @@ namespace wl
 		return &m_list[index];
 	}
 
-	void PointLightList::Bind(int slot) const
+	void PointLightList::Bind(int slot)
 	{
-		if (m_list.size() == 0) return;
-
-		D3D11_MAPPED_SUBRESOURCE msub = {};
-
-		ASSERT_HR(
-			DXContext::Instance->m_context->Map(m_buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &msub),
-			"Failed to update lights."
-		);
-
-		CopyMemory(msub.pData, m_list.data(), m_list.size() * sizeof(PointLight));
-		DXContext::Instance->m_context->Unmap(m_buffer.Get(), 0);
-
+		SetData();
 		DXContext::Instance->m_context->PSSetShaderResources(slot, 1, m_view.GetAddressOf());
 	}
 
 	size_t PointLightList::Size() const
 	{
 		return m_list.size();
+	}
+
+	void PointLightList::SetData()
+	{
+		if (m_prevElementCount != m_list.size())
+		{
+			D3D11_MAPPED_SUBRESOURCE msub = {};
+			if (m_list.size() == 0)
+			{ // If list is empty just put one empty byte in the buffer.
+				msub.pData = new byte[1]{ 0 };
+			}
+			if (m_list.size() > m_currentAllocationSize)
+			{ // If the list size is larger than the current buffer size then make a new buffer.
+				MakeBuffer();
+			}
+			else
+			{
+
+				ASSERT_HR(
+					DXContext::Instance->m_context->Map(m_buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &msub),
+					"Failed to update lights."
+				);
+
+				CopyMemory(msub.pData, m_list.data(), m_list.size() * sizeof(PointLight));
+				DXContext::Instance->m_context->Unmap(m_buffer.Get(), 0);
+				MakeView();
+			}
+		}
+	}
+
+	void PointLightList::MakeBuffer()
+	{
+		if (m_list.size() > m_currentAllocationSize) m_currentAllocationSize += m_preAllocationSize;
+
+		D3D11_BUFFER_DESC sbufferDesc = {  };
+		sbufferDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		sbufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+		sbufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		sbufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+		sbufferDesc.ByteWidth = sizeof(PointLight) * m_currentAllocationSize;
+		sbufferDesc.StructureByteStride = sizeof(PointLight);
+
+		D3D11_SUBRESOURCE_DATA sbufferData{};
+		sbufferData.pSysMem = m_list.data();
+
+		ASSERT_HR(
+			DXContext::Instance->m_device->CreateBuffer(&sbufferDesc, &sbufferData, &m_buffer),
+			"Unable to create buffer."
+		);
+
+		MakeView();
+	}
+
+	void PointLightList::MakeView()
+	{
+		// Make view
+		D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+		srvDesc.Format = DXGI_FORMAT_UNKNOWN;
+		srvDesc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+		srvDesc.Buffer.FirstElement = 0;
+		srvDesc.Buffer.NumElements = m_list.size();
+
+		ASSERT_HR(
+			DXContext::Instance->m_device->CreateShaderResourceView(m_buffer.Get(), &srvDesc, &m_view),
+			"Unable to get shader resource view."
+		);
 	}
 
 }
--- Direct3DRenderer/source/PointLightList.h
+++ Direct3DRenderer/source/PointLightList.h
@@ -15,12 +15,18 @@ namespace wl
 		void Add(PointLight& light);
 		void Remove(int index);
 		PointLight *Get(int index);
-		void Bind(int slot) const;
+		void Bind(int slot);
 		size_t Size() const;
 	private:
+		void SetData();
+		void MakeBuffer();
+		void MakeView();
 		std::vector<PointLight> m_list;
 		wrl::ComPtr<ID3D11Buffer> m_buffer;
 		wrl::ComPtr<ID3D11ShaderResourceView> m_view;
+		int m_prevElementCount{ 0 };
+		const int m_preAllocationSize{ 5 };
+		int m_currentAllocationSize{ 0 };
 	};
 
 }
