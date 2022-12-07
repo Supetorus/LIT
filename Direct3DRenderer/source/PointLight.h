33227e34e5e8af9618baabc667beec0dc4ec9d37
Supetorus <littlewilliamthomas@gmail.com>
2022-12-07 15:35:03 -0700

    Changes pointlight struct so multiple lights are working.

--- Direct3DRenderer/source/PointLight.h
+++ Direct3DRenderer/source/PointLight.h
@@ -9,7 +9,7 @@ namespace wl
 		dx::XMFLOAT4 ambient{ 0.2f, 0.2f, 0.2f, 0 };
 		dx::XMFLOAT4 diffuse{ 1.0f, 1.0f, 1.0f, 1.0f };
 		dx::XMFLOAT4 specular{ 1.0f, 1.0f, 1.0f, 1.0f };
-		alignas(16) dx::XMFLOAT3 position{ 0, 0, 0 };
+		/*alignas(16)*/ dx::XMFLOAT3 position{ 0, 0, 0 };
 		dx::XMFLOAT3 attenuation{ 1.0f, 0.045f, 0.0075f };
 		float range{ 50 };
 	};
